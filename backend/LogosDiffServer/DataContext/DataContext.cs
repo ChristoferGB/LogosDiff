f9dcaa0ca6d785b7ca8bee88af1d5690bde4a643
Christofer Bernardo <christofergaldino2@hotmail.com>
2026-04-08 09:01:03 +0100

    feat: creates inital structure and adds EF Core dependencies

---
+++ backend/LogosDiffServer/DataContext/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
