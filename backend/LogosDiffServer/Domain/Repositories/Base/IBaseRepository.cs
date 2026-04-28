9ec73928da04b9aa1c0b1339f7f0f74d2d91337b
Christofer Bernardo <christofergaldino2@hotmail.com>
2026-04-28 12:15:03 +0100

    feat: creates project to fill database based on json files

---
+++ backend/LogosDiffServer/Domain/Repositories/Base/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Repositories.Base
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T?> GetItemByIdAsync(Guid id);
+        List<T> GetAllItems();
+        void AddItem(T item);
+        void AddRange(IEnumerable<T> items);
+        void UpdateItem(T item);
+        void DeleteItem(T item);
+        Task SaveChangesAsync();
+    }
+}
